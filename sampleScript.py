8b4a5e54f962916b23c790cddef867185ac08f5e
Daniel De Biasio <danieldb@live.ca>
2020-05-13 11:17:41 -0700

    i modified the second matrix

--- sampleScript.py
+++ sampleScript.py
@@ -2,6 +2,6 @@ import numpy as np
 import pandas as pd
 
 my_matrix = np.eye(4)
-my_second_matrix = np.array[[1,2,3,4], [2,3,4,5], [4,0,9,8], [2,3,6,6]]
+my_second_matrix = np.array[[1,2,3,4], [2,3,4,5], [1,2,1,2], ['hello', 'goodbye', 'boris', 'arron']]
 
 print(my_matrix)
