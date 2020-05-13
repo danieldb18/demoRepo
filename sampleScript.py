36e790d244c89bd630d38e62f18fa0b7f7995db8
Daniel De Biasio <danieldb@live.ca>
2020-05-13 11:41:58 -0700

    added printouts

--- sampleScript.py
+++ sampleScript.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 
 my_matrix = np.eye(5)
-my_second_matrix = np.array[[1,2,3,4], [2,3,4,5], [8,8,8,8], ['hello', 'goodbye', 'boris', 'arron']]
+my_second_matrix = np.array[[1,2,3,4], [2,3,4,5], [8,8,8,8], ['hello', 'goodbye', 'aaron', 'danie']]
 
 print(my_matrix)
+print('boo')
